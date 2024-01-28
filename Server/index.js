2c8d6c6ab6cb50dda17b47769b55a34325653ba4
zakaria dorhmi <dorhmizakaria@gmail.com>
2024-01-29 00:15:44 +0100

    connect MongoDB

--- Server/index.js
+++ Server/index.js
@@ -3,16 +3,36 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from "mongoose";
 import multer from 'multer';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv'
+// import authRoutes from './Routes/authRoutes';
+// import userRoutes from "./Routes/userRoutes";
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use('/assets' , express.static(path.join(__dirname, "public/assets")));
 
 app.get('/' , (req,res) => {
     res.status(234).send("Welcom")
 })
 
-const port= process.env.PORT || 3001;
+// app.use('/auth' , authRoutes);
+// app.use('/users' , userRoutes);
 
-app.listen(port , () => console.log(`App is listening to port : ${port}`))
+const PORT = process.env.PORT;
+const URL = process.env.MONGO_URL;
+
+mongoose.connect(URL)
+.then(()=>{
+    console.log('App connected to database');
+    app.listen(PORT , () => console.log(`App is listening to port : ${PORT}`))
+})
+.catch((error) => {
+    console.log(error);
+})
