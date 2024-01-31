b44259e30b5ea6a8b369acb808270066a215b174
zakaria dorhmi <dorhmizakaria@gmail.com>
2024-01-31 21:19:35 +0100

    loginPage

--- Client/src/pages/LoginPage.jsx
+++ Client/src/pages/LoginPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const LoginPage = () => {
 return (
-    <div>LoginPage</div>
+    <section className='login-section'>
+        <div className='login-content'>
+            <p className='login-header'>Login</p>
+            <form className='login-form'>
+                <input className='login-input' type="email" placeholder='Email' />
+                <input className='login-input' type="text" placeholder='Password'/>
+                <button className='login-button' type='submit'>Login</button>
+            </form>
+            <p className='login-footer'>Not a Member ? <Link to={"/register"}>register</Link></p>
+        </div>
+    </section>
 )
 }
 
